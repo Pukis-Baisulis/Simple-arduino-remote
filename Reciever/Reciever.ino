f2069cb8cfa796eabafe2294d2434a9b29e520b5
Pukis-Baisulis <87567289+Pukis-Baisulis@users.noreply.github.com>
2023-01-10 23:23:24 +0200

    0002

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -1,24 +1,82 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
 
+
+#define CH1_PIN 6
+#define CH2_PIN 5
+#define CH3_PIN 9
+#define ANTENA_PIN 11
+
 RH_ASK driver;
+
+void erroredOut();
 
 void setup()
 {
     Serial.begin(9600);	// Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(CH1_PIN, OUTPUT);
+    pinMode(CH2_PIN, OUTPUT);
+    pinMode(CH3_PIN, OUTPUT);
+    while(!driver.init())
+    {
+      Serial.println("init failed");
+      erroredOut();
+    }
+  Serial.println("Nice");
 }
 
 void loop()
 {
-    uint8_t buf[12];
-    uint8_t buflen = sizeof(buf);
-    if (driver.recv(buf, &buflen)) // Non-blocking
-    {
-      int i;
-      // Message with a good checksum received, dump it.
-      Serial.print("Message: ");
-      Serial.println((char*)buf);         
-    }
+  uint8_t buf[3];
+  uint8_t buflen = sizeof(buf);
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  {
+    Serial.println((char*)buf);
+  }
+    
+  
+  if((char*)buf == "500") //CH1 active
+  {
+    digitalWrite(CH1_PIN, HIGH);
+    digitalWrite(CH1_PIN, LOW);      
+    digitalWrite(CH1_PIN, LOW);
+  }
+  if((char*)buf == "005") // CH3 active
+  {
+    digitalWrite(CH1_PIN, LOW);
+    digitalWrite(CH1_PIN, LOW);      
+    digitalWrite(CH1_PIN, HIGH);
+  }
+  if((char*)buf == "050") // CH2 active 
+  {
+    digitalWrite(CH1_PIN, LOW);
+    digitalWrite(CH1_PIN, HIGH);      
+    digitalWrite(CH1_PIN, LOW);
+  }
+  else // none active
+  {
+    digitalWrite(CH1_PIN, LOW);
+    digitalWrite(CH1_PIN, LOW);      
+    digitalWrite(CH1_PIN, LOW);
+  }
+
+}
+
+
+void erroredOut()
+{
+  int c = 0;
+  while(c<5)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    c++;
+  }
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -3,22 +3,91 @@
 
 RH_ASK driver;
 
-const char *msg1 = "UP";
-const char *msg2 = "NEUTRAL";
-const char *msg3 = "DOWN";
+#define MIN_VOLTAGE 7200
+
+#define UP_PIN 6
+#define MID_PIN 5
+#define DOWN_PIN 4
+#define ANTENA_PIN 12
+#define LED 13
+#define BATTERY_PIN A0
+
+//const char *msg1 = "500";
+//const char *msg2 = "050";
+//const char *msg3 = "005";
+//const char *msgNeutral = "000";
+
+int voltage;
+bool btn_1;
+bool btn_2;
+bool btn_3;
+
+void erroredOut();
 
 void setup()
 {
-    Serial.begin(9600);	  // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  Serial.begin(9600);	// Debugging only
+  while(!driver.init())
+  {
+    Serial.println("init failed");   
+    erroredOut();
+  }    
+  Serial.println("Transmitter confirmed");
+               
 }
 
 void loop()
 {
-    const char *msg = "Hello World!";
-    driver.send((uint8_t *)msg, strlen(msg));
-    Serial.println(msg);
-    driver.waitPacketSent();
+  // voltage reading and setting led on if voltage is low
+  voltage = analogRead(BATTERY_PIN)*2;
+  if(voltage < MIN_VOLTAGE)
+    digitalWrite(LED, HIGH);
+  else
+    digitalWrite(LED, LOW);
+
+  btn_1 = digitalRead(UP_PIN);
+  btn_2 = digitalRead(MID_PIN);
+  btn_3 = digitalRead(DOWN_PIN);
+  
+  if(btn_1 && btn_2 && btn_3) // none
+    const char *msg = "000";
+  if(!btn_1 && !btn_2 && !btn_3)// none
+    const char *msg = "000";
+  if(btn_1 && btn_2 && btn_3)// none
+    const char *msg = "000";
+  if(btn_1 && btn_2 && btn_3)// none
+    const char *msg = "000";
+
+    
+  if(btn_1 && !btn_2 && !btn_3) // up
+    const char *msg = "500";
+
+  if(!btn_1 && btn_2 && !btn_3) // mid
+    const char *msg = "050";
+
+  if(!btn_1 && !btn_2 && btn_3) // down
+    const char *msg = "005";
+
+  driver.send((char *)msg, strlen(msg));
+  Serial.println(msg);
+  driver.waitPacketSent();
+  
+  delay(100);
+}
+
+void erroredOut()
+{
+  int c = 0;
+  while(c<5)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    c++;
+  }
 }
